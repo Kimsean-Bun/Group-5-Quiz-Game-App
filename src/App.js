f5e94899e19fa2e1871339b48179ee84e364461e
TANGHuyming <2023277tang@aupp.edu.kh>
2025-11-10 09:50:46 +0700

    added homepage 40% done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
       <div className="app-container">
         <Header />
+        <Home />
         <Footer />
       </div>
   );
