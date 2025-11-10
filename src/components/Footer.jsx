b74ce14536f18bfd7d889d941475e1fa614065fa
TANGHuyming <2023277tang@aupp.edu.kh>
2025-11-10 08:57:51 +0700

    made footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+    return (
+        <div className="footer-container">
+            <nav className="nav-container">
+                <a href="">Home</a>
+                <a href="">ScoreBoard</a>
+                <a href="">About Us</a>
+                <a href="">Super Secret</a>
+            </nav>
+
+            <h5>&copy; 2025 by Tang Huyming, Tiangkimhong Hoeu, and Kimsean. All Rights Reserved</h5>
+
+            <div className="address-container">
+                <h2>Reach Out To Us: </h2>
+                <p><strong>Address</strong>: #278H, Street 201R, Kroalkor Village, Sangkat Kilometer 6, Khan Russey Keo, Phnom Penh, Cambodia</p>
+                <p><strong>Email</strong>: <a href="mailto:triviabonanza@aupp.edu.kh">triviabonanza@aupp.edu.kh</a></p>
+                <p><strong>Telephone</strong>: <a href="tel+85515674833">(+855) 15 674 833</a></p>
+            </div>
+
+            <div className="logo-container">
+                <h2>LOGO</h2>
+            </div>
+        </div>
+    );
+}
