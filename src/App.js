8f9850f247b9646ef43a774062b1d48d4b74a6d6
kimseanbun <bunkimsean6@gmail.com>
2025-11-12 08:35:13 +0700

    HomePage Structure 50% Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import GamePage from './components/GamePage';
 
 function App() {
+
+  const sampleQuestion = {
+    id: 1,
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin"],
+    correctAnswer: "Paris",
+  };
+
   return (
-      <div className="app-container">
-        <Header />
-        <Home />
-        <Footer />
-      </div>
+    <div className="app-container">
+      <Header />
+      <Home />
+      <GamePage
+          questionData={sampleQuestion}
+          user="USER#1"
+          onNextQuestion={() => alert("Next question!")}
+        />
+      <Footer />
+    </div>
   );
 }
 
